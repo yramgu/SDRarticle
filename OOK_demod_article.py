a480e7789004ccaf757d676b87eac9868f1a1534
yramgu <46901691+yramgu@users.noreply.github.com>
2019-02-28 10:32:27 +0000

    Add files via upload
    Commit of code and rev1 of PDF documents

---
+++ OOK_demod_article.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+#
+# Example of demodulation of an OOK signal
+# Y.Ramgulam - 2019
+#
+# requirement: numpy, scipy, matplotlib
+
+import OOK_demod_functions as functions
+
+import numpy as np
+import scipy.signal
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import time
+
+# --- Constants
+bitrate         = 5000
+DecimationRatio = 10
+
+# --- read wav_file
+Fs, data = wavfile.read('./_WAV/keyfob_iq_ook.wav')
+data = data/np.amax(data)
+
+I_data = data[:,0]
+Q_data = data[:,1]
+
+I_data = I_data[130000:185022] # I'm only interested in this part 
+Q_data = Q_data[130000:185022] # I'm only interested in this part
+
+# --- This is the original complex IQ stream
+complex_data = I_data + 1j*Q_data
+
+# --- Decimate data to improve SNR and reduce computing load
+# We decimate by 10 i.e. 2e6/10 = 200kHz output bandwidth
+decimated_data = scipy.signal.decimate(complex_data, DecimationRatio, ftype='iir')
+
+# This is just for the sake of plotting, to show the difference
+# between original signal and decimated signal on the same scale
+interpolated_data = scipy.signal.resample_poly(decimated_data, DecimationRatio,1) 
+
+# --- demodulate (extract magnitude)
+data_magnitude = np.abs(decimated_data)
+
+# --- Matched filtering
+samples_per_bit = int(Fs/DecimationRatio/bitrate) # Calculate number of samples per bit
+reference_pulse = np.repeat([1], samples_per_bit) # Create reference pulse of length "samples_per_bit"
+matched_data = scipy.signal.correlate(data_magnitude, reference_pulse, mode='same') / samples_per_bit # Correlate
+
+# --- Eliminate DC component and slice data resulting in samples between +/-0.5
+matched_data /= np.amax(matched_data)
+ac_data = matched_data - np.average(matched_data)
+
+# --- synchronize bit stream
+synced_data, clock = functions.clock_recovery(data=ac_data, DataRate=bitrate, SamplingRate=Fs/DecimationRatio)
+
+# --- Recover bits
+recovered_bits = []
+for bit in synced_data:
+    if bit > 0:
+        recovered_bits.append(1)
+    else:
+        recovered_bits.append(0)
+
+print("recovered bits = ", recovered_bits)
+
+# --- frame synchronisation : Look for synchronisation word
+SynchroFrameIndex, PayloadStart, SynchroFrameResult = functions.search_sync(recovered_bits,'DB6A', sync_threshold=12)
+
+# --- payload decoding
+payload = functions.payload_extraction(recovered_bits, PayloadStart, payload_size=9)
+
+# --- Plot results
+fig, (ax_iq, ax_mag) = plt.subplots(2, 1, sharex=False)
+fig2, (ax_match, ax_clocksync, ax_framesync) = plt.subplots(3, 1, sharex=False)
+
+ax_iq.plot(I_data+2.5, label='I (Fs=2Msps)')
+ax_iq.plot(Q_data, label='Q (Fs=2Msps)')
+ax_iq.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=1, mode=None, borderaxespad=0.)
+
+ax_mag.plot(np.abs(complex_data), label='demodulated signal - before decimation')
+ax_mag.plot(np.abs(interpolated_data), label='demodulated signal - after x10 decimation')
+ax_mag.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=1, mode=None, borderaxespad=0.)
+
+ax_match.plot(data_magnitude, label='Demodulated data')
+ax_match.plot(matched_data, label='match-filtered data')
+ax_match.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=1, mode=None, borderaxespad=0.)
+
+ax_clocksync.plot(ac_data, label='dc-blocked data')
+ax_clocksync.plot(clock,synced_data, 'ro', label='clock-synchronized data')
+ax_clocksync.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=1, mode=None, borderaxespad=0.)
+
+ax_framesync.plot(SynchroFrameResult, label='SynchroFrame correlation')
+ax_framesync.plot(SynchroFrameIndex,SynchroFrameResult[SynchroFrameIndex], 'ro', label='clock-synchronized data')
+ax_framesync.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=1, mode=None, borderaxespad=0.)
+
+fig2.tight_layout()
+fig.tight_layout()
+plt.show()
+
+
---
+++ OOK_demod_functions.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+#
+# RTL FM Receiver - Python based realtime RF signal processing experiment
+#  @edy555 2016
+# requirement: numpy, scipy, pyaudio, pyrtlsdr
+
+import numpy as np
+import scipy.signal
+
+# ----------------------------------------------------------------
+
+def clock_recovery(data, DataRate, SamplingRate):
+
+    sps             = int(SamplingRate/DataRate)
+    clock           = []
+    synced_data     = []
+    error           = 1
+    index           = 0
+    epsilon         = 0
+    Kp              = -sps/2
+
+    while (index*sps + epsilon) < len(data) - sps :
+
+        # This is Gardner formula
+        error = (data[index*sps + epsilon] - data[(index-1)*sps + epsilon]) * data[index*sps - sps//2 + epsilon] 
+        
+        # We apply a gain to the error 
+        epsilon += int(np.round(error*Kp))
+
+        # and we store the result
+        clock.append(int(index*sps + epsilon))
+        synced_data.append(data[int(index*sps + epsilon)])
+
+        index += 1
+    
+    return synced_data, clock
+
+# ------------------------------------------------------------------------
+
+def search_sync(data, sync_word, preamble_pattern = 'AA', nb_preamble_bytes=2, sync_threshold=12):
+    """
+        data                : array containing the data
+        sync_word           : hex string, MSB first, WITHOUT '0x'. Example : AE55 for 0xAE 0x55
+        preamble_pattern    : hex string, MSB first, WITHOUT '0x'. Example : AA for 0xAA
+        nb_preamble_bytes   : number of preamble bytes to consider
+        sync_threshold      : threshold level to validate correlation
+  
+        returns : index of sync if success
+      
+    """
+    # Variables
+    sync_string = nb_preamble_bytes*preamble_pattern + sync_word
+    sync_number = int(sync_string, 16)
+    sync_index  = 0
+
+    # Format HEX sync word & preamble into array of bits
+    spec            = '{fill}{align}{width}{type}'.format(fill='0', align='>', width=len(sync_string)//2*8, type='b')
+    sync_bit_string = format(sync_number,spec)
+    
+    sync_bit_list   = [0]*len(sync_bit_string)
+    for i in range(0,len(sync_bit_string)):
+        sync_bit_list[i] = int(sync_bit_string[i])
+
+    # correlation
+    synchronisation_sequence = np.repeat(sync_bit_list, 1) # Generation of reference sequence
+    correlated_data = scipy.signal.correlate(data, synchronisation_sequence, mode='same')
+
+    # decision
+    for i in range(1,len(correlated_data)-1):
+        # if we have a peak above the threshold
+        if (correlated_data[i] > sync_threshold):
+            # We check that the neighbouring peaks are sufficiently lower
+            if (correlated_data[i] - correlated_data[i-1] > sync_threshold) and (correlated_data[i] - correlated_data[i+1] > sync_threshold) :
+                sync_index = i
+                payload_start = i + int(np.ceil(len(synchronisation_sequence)/2))
+                print("SYNC found at index ", sync_index,"/ ",len(correlated_data))
+                print("PAYLOAD starts at index ", payload_start,"/ ",len(correlated_data))
+                print("Correlation level is ", correlated_data[i] - correlated_data[i-1])
+            else :
+                pass
+        else:
+            pass
+          
+    return sync_index, payload_start, correlated_data
+
+# ------------------------------------------------------------
+
+def payload_extraction(data, sync_index, payload_size=0):
+
+    payload_hex     = [0]*payload_size
+    payload_data    = 0
+
+    for j in range(0, payload_size):
+
+        payload_data = 0
+        k = 7
+
+        for i in range(sync_index + j*8, sync_index + j*8 + 8):
+            payload_data += int(data[i]) << k
+            k -= 1
+            
+        payload_hex[j] = hex(payload_data)
+          
+    print("Payload = ",payload_hex)
+
+    return payload_hex
